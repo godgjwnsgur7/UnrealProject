c99d400dba928d226a1c8a3abf55bfce57c17b28
Jun <godgjwnsgur7@naver.com>
2024-12-20 16:18:38 +0900

    editorconfig

---
+++ JunStudyProject/Source/JunStudyProject/ActorTest.cpp
@@ -0,0 +1,25 @@
+﻿#include "ActorTest.h"
+
+// Sets default values
+AActorTest::AActorTest()
+{
+ 	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+
+}
+
+// Called when the game starts or when spawned
+void AActorTest::BeginPlay()
+{
+	// 한글 주석 테스트
+	Super::BeginPlay();
+	
+}
+
+// Called every frame
+void AActorTest::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+}
+
