63df867ab0c1389cd3722ca71897c3464b2013d0
Jun <godgjwnsgur7@naver.com>
2024-12-26 17:03:12 +0900

    Location, Scale, Rotation 테스트

--- JunStudyProject/Source/JunStudyProject/JunActor.cpp
+++ JunStudyProject/Source/JunStudyProject/JunActor.cpp
@@ -12,12 +12,39 @@ AJunActor::AJunActor()
  	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
 	PrimaryActorTick.bCanEverTick = true;
 
+	/*
 	Box = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("Box"));
 
 	ConstructorHelpers::FObjectFinder<UStaticMesh> FindMesh(TEXT("/Script/Engine.StaticMesh'/Game/_Jun/SM_ChamferCube.SM_ChamferCube'"));
 	if (FindMesh.Succeeded())
 	{
 		Box->SetStaticMesh(FindMesh.Object);
+	}
+	*/
+
+	Body = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("Body"));
+	RootComponent = Body;
+	Body->SetRelativeScale3D(FVector(2, 3, 0.5f));
+
+	Wing = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("Wing"));
+	Wing->SetupAttachment(Body);
+	Wing->SetRelativeLocationAndRotation(FVector(55, -72, 88), FRotator(0, 90, 0));
+	Wing->SetRelativeScale3D(FVector(3.75f, 0.25f, 0.5f));
+
+	Head = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("Head"));
+	Head->SetupAttachment(Body);
+	Head->SetRelativeLocationAndRotation(FVector(105, 36, 48), FRotator(0, 0, 0));
+	Head->SetRelativeScale3D(FVector(0.25f, 0.25f, 0.25f));
+
+	Wing->SetRelativeLocation(FVector(0, 0, 0));
+	Head->SetRelativeLocation(FVector(50, 0, 0));
+
+	ConstructorHelpers::FObjectFinder<UStaticMesh> FindMesh(TEXT("/Script/Engine.StaticMesh'/Game/_Jun/SM_ChamferCube.SM_ChamferCube'"));
+	if (FindMesh.Succeeded())
+	{
+		Body->SetStaticMesh(FindMesh.Object);
+		Wing->SetStaticMesh(FindMesh.Object);
+		Head->SetStaticMesh(FindMesh.Object);
 	}
 }
 
@@ -57,6 +84,8 @@ void AJunActor::Tick(float DeltaTime)
 		// SetActorLocation(NewLocation);
 
 		AddActorWorldOffset(DirectionVector * Distance);
+
+
 	}
 }
 
--- README.md
+++ README.md
@@ -1,5 +1,5 @@
-# Unreal Study Project
- 
+# Unreal Study Project (Version 5.5)
+
 - 언리얼 입문
 - 블루프린트
 - 리플렉션, 프로퍼티
